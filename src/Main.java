c94c0bfaaa01b318b72eebb3b593097df111b6c7
Stone He <stone.he@ericsson.com>
2016-09-19 14:18:58 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import com.i706.world.addition;
+
+public class Main {
+
+    public static void main(String[] args) {
+        addition jia = new addition();
+        int result = jia.add(7,8);
+        System.out.println("Hello World!" + result);
+    }
+}
---
+++ src/com/i706/world/addition.java
@@ -0,0 +1,10 @@
+package com.i706.world;
+
+/**
+ * Created by ezhuohe on 9/19/2016.
+ */
+public class addition {
+    public int add (int a, int b){
+        return a+b;
+    }
+}
