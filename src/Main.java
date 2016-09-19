e9ec5228bd954819c08463b369e2adfa0a59ec43
drunkensailor2010 <stone.he@ericsson.com>
2016-09-19 14:21:13 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         addition jia = new addition();
-        int result = jia.add(7,8);
+        int result = jia.add(70,8);
         System.out.println("Hello World!" + result);
     }
 }
